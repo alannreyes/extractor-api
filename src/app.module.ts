ff38c35a08a0cf1499a54c84ef355e9ffbcece6e
Alann Reyes <alannreyesj@gmail.com>
2025-06-26 06:14:11 -0500

    WIP: Setting up MySQL and TypeORM configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
-import { Module } from '@nestjs/common';
+﻿import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ThrottlerModule } from '@nestjs/throttler';
-import { PdfParserModule } from './pdf-parser/pdf-parser.module';
+import { ClaimsExtractorModule } from './claims-extractor/claims-extractor.module';
+import { ClaimExtract } from './entities/claim-extract.entity';
 import configuration from './config/configuration';
 
 @Module({
@@ -10,15 +12,30 @@ import configuration from './config/configuration';
       isGlobal: true,
       load: [configuration],
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get('database.host'),
+        port: config.get('database.port'),
+        username: config.get('database.username'),
+        password: config.get('database.password'),
+        database: config.get('database.database'),
+        entities: [ClaimExtract],
+        synchronize: false,
+        logging: false,
+      }),
+    }),
     ThrottlerModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ([{
-        ttl: config.get<number>('THROTTLE_TTL', 60) * 1000, // Convertir a milisegundos
+        ttl: config.get<number>('THROTTLE_TTL', 60) * 1000,
         limit: config.get<number>('THROTTLE_LIMIT', 10),
       }]),
     }),
-    PdfParserModule,
+    ClaimsExtractorModule,
   ],
 })
 export class AppModule {}
